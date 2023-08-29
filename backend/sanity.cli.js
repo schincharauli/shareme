019b13eb231b85dfe8dbdf1f6c705be770a32e26
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-29 14:06:54 +0400

    project setup

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c8nb8s7o',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/index.js
@@ -0,0 +1,2 @@
+export const schemaTypes = []
+import user from './user'
