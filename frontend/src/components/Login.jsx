7e528d57b274f1e324bf80fdc3deb078960701e6
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-30 14:42:51 +0400

    added video on the login page

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+const Login = () => {
+  return (
+    <GoogleOAuthProvider clientId="">
+      <div className="flex justify-start items-center flex-col h-screen">
+        <div className="relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default Login;
