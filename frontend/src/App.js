9f9650be9b0ea24af4fb5977d4446255d7e537c5
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-30 17:40:01 +0400

    added google auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <GoogleOAuthProvider clientId="353672677502-2pmnqv8mug225vj83ikgonsnpfn2o9ek.apps.googleusercontent.com">
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
   );
 };
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,27 +1,58 @@
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import dotenv from "react-dotenv";
 
 const Login = () => {
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObject));
+    const { name, googleId, imageUrl } = response.profileObject;
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+  };
   return (
-    <GoogleOAuthProvider clientId="">
-      <div className="flex justify-start items-center flex-col h-screen">
-        <div className="relative w-full h-full">
-          <video
-            src={shareVideo}
-            type="video/mp4"
-            loop
-            controls={false}
-            muted
-            autoPlay
-            className="w-full h-full object-cover"
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} alt="logo" width="130px" />
+        </div>
+        <div className="shadow-2xl">
+          <GoogleLogin
+            render={(renderProps) => (
+              <button
+                type="button"
+                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <FcGoogle className="mr-4" /> Sign in with google
+              </button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
           />
         </div>
       </div>
-    </GoogleOAuthProvider>
+    </div>
   );
 };
 
